01f9cb5903754dbd37559817cf8dafd703a294cc
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-02-18 13:48:07 +0100

    update favicon
    add postcss
    
    create component for fancytext

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
+	safelist: [
+		{
+			pattern: /text-(.*)-([1-8]00)/,
+			variants: ['hover', 'dark'],
+		},
+	],
 	plugins: [],
 }
