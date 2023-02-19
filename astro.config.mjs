9740e4ff0d20d679948729bf67300dee8a54ad41
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-02-19 19:10:46 +0100

    fix menu not working well on large screens

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://powerd6.org',
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()]
 });
